a66150753504f578b822963c898d088eabde6708
ESchanz <schanz.10@buckeyemail.osu.edu>
2015-04-12 16:41:22 -0400

    Added application authentication model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412195419) do
+ActiveRecord::Schema.define(version: 20150412203345) do
+
+  create_table "applications", force: true do |t|
+    t.string   "Name"
+    t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "cycling_profs", force: true do |t|
     t.integer  "CycID"
