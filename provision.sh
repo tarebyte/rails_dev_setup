6f323eb7f69c5851c9916d3564418f8049424422
Brad Arner <arner.john@gmail.com>
2015-04-11 23:10:04 -0400

    Adding Redis and ImageMagick to provision script

--- provision.sh
+++ provision.sh
@@ -2,7 +2,16 @@ apt-add-repository ppa:brightbox/ruby-ng
 add-apt-repository ppa:chris-lea/node.js
 apt-get -y update
 
-apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libxml2
+apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libxml2 tcl8.5 imagemagick
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+make install
+cd
+
 
 apt-get -y install ruby2.2 ruby2.2-dev
 
